75b4902f442d389172097fbd3bd96e5e3c434226
irfanhanif <if.irfanhanif@gmail.com>
2018-07-11 13:14:12 +0700

    Shuffling deck; shuffle, Enum, immutability.

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,13 +1,22 @@
-## Biasanya '' bisa digunakan tetapi
-# konvensinya menggunakan "" (dua petik) dalam string
+# Enum adalah library built in pada elixir
+## tidak perlu melakukan import, langsung bisa digunakan
 
-## Bisa menggunakan "recompile" jika mengubah kode tanpa
-# harus keluar ketika sedang menggunakan terminal
+# Pada terminal...
+## deck = Cards.create_deck
+## Cards.shuffle(deck)
+
+## Immutability: tidak mengganti isi data pada variabel asli
+# melainkan melakukan copy dan do something pada
+# hasil copy tersebut
 
 defmodule Cards do
 
   def create_deck do
     ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
 
 end
